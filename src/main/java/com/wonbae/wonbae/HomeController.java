187946288f51849fbf869ceb41844c211dc00c2f
HOME-PC\chase03 <thoughtlib@ichase.co.kr>
2023-06-22 10:07:53 +0900

    init

---
+++ src/main/java/com/wonbae/wonbae/HomeController.java
@@ -0,0 +1,12 @@
+package com.wonbae.wonbae;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    } 
+}
---
+++ src/main/java/com/wonbae/wonbae/WonbaeApplication.java
@@ -0,0 +1,13 @@
+package com.wonbae.wonbae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonbaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonbaeApplication.class, args);
+	}
+
+}
