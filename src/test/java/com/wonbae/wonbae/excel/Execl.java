30bb92acdd030fe04a4b6a8d1a59c1bbeda0137a
전원배 <thoughtlib@ichase.co.kr>
2023-07-31 13:28:20 +0900

    t

---
+++ src/test/java/com/wonbae/wonbae/excel/Execl.java
@@ -0,0 +1,15 @@
+package com.wonbae.wonbae.excel;
+
+import org.junit.jupiter.api.Test;
+
+public class Execl {
+
+    final static String VWORLD_API_KEY = "08C1CFAB-C788-309D-9DFA-4E96F2E3E60B";
+
+    @Test
+    public void test() {
+        StringBuilder urlBuilder = new StringBuilder("http://api.vworld.kr/req/address?service=address&request=getCoord&key=" + VWORLD_API_KEY); /*URL*/
+
+        
+    }
+}
